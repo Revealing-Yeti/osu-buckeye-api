91432b5fab909d8c915a98c4eada07710816bd8b
donkadoom <borkar.21@buckeyemail.osu.edu>
2025-04-23 12:22:02 -0400

    added Auth

--- osu-buckeye-api/Program.cs
+++ osu-buckeye-api/Program.cs
@@ -1,8 +1,31 @@
+using Jet.Piranha.Api.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string authority = builder.Configuration["Auth0:Authority"] ??
+    throw new ArgumentException("Auth0:Authority");
+
+string audience = builder.Configuration["Auth0:Audience"] ??
+    throw new ArgumentException("Auth0:Audience");
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => {
+    options.Authority = authority;
+    options.Audience = audience;
+});
+
+builder.Services.AddAuthorization(options => {
+    options.AddPolicy("delete:catalog", policy => policy.RequireAuthenticatedUser().RequireClaim("scope", "delete:catalog"));
+});
+
 
 //ADD AFTER builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(options =>
@@ -33,7 +56,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors();
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAuthorization();
 
 app.MapControllers();
